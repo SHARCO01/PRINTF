602bc90a7bfde8ed638892afb785d207243bd194
Imad Laghraib <ilaghrai@c2r8p2.1337.ma>
2025-11-21 00:29:54 +0100

    push

--- ft_printf.c
+++ ft_printf.c
@@ -6,53 +6,60 @@
 /*   By: ilaghrai <ilaghrai@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 21:48:17 by ilaghrai          #+#    #+#             */
-/*   Updated: 2025/11/18 03:01:38 by ilaghrai         ###   ########.fr       */
+/*   Updated: 2025/11/21 00:25:52 by ilaghrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	formats(va_list arg, const char format)
+{
+	int	count;
+
+	count = 0;
+	if (format == 'c')
+		count += print_char(va_arg(arg, int));
+	else if (format == 's')
+		count += print_string(va_arg(arg, char *));
+	else if (format == 'd' || format == 'i')
+		count += print_int(va_arg(arg, int));
+	else if (format == 'u')
+		count += print_unsigned_int(va_arg(arg, unsigned int));
+	else if (format == 'x')
+		count += print_lw_hexa(va_arg(arg, unsigned int));
+	else if (format == 'X')
+		count += print_up_hexa(va_arg(arg, unsigned int));
+	else if (format == 'p')
+		count += print_address(va_arg(arg, void *));
+	else if (format == '%')
+		count += write(1, "%", 1);
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
 	int		i;
-	int		count;
+	int		printed;
 	va_list	arg;
 
-	va_start(arg, format);
 	i = 0;
-	count = 0;
+	printed = 0;
 	if (!format)
 		return (-1);
+	va_start(arg, format);
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				count += print_char(va_arg(arg, int));
-			else if (format[i] == 's')
-				count += print_string(va_arg(arg, char *));
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_int(va_arg(arg, int));
-			else if (format[i] == 'u')
-				count += print_unsigned_int(va_arg(arg, unsigned int));
-			else if (format[i] == 'x')
-				count += print_lw_hexa(va_arg(arg, unsigned int));
-			else if (format[i] == 'X')
-				count += print_up_hexa(va_arg(arg, unsigned int));
-			else if (format[i] == 'p')
-				count += print_address(va_arg(arg, void *));
-			else if (format[i] == '%')
-				count += write(1, "%", 1);
-			else if (format[i + 1] != '\0')
-				break ;
+			if (format[i] == '\0')
+				return (-1);
+			printed += formats(arg, format[i]);
 		}
 		else
-			count += write(1, &format[i], 1);
-			
+			printed += print_char(format[i]);
 		i++;
 	}
 	va_end(arg);
-	return (count);
+	return (printed);
 }
---
+++ main0.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "libdeflate.h"
+#include <stdio.h>
+
+/*int ooo(char a,int v, ...)
+{
+        va_list m;
+        va_start(m,v);
+
+        printf("%p\n", m->reg_sav,e_area);
+        printf("%p\n", m->overflow_arg_area);
+        printf("%d\n", *(int*)(m->reg_save_area + 192));
+        printf("%d\n", *(int*)(m->overflow_arg_area ));
+        printf("%d\n", m->gp_offset);
+
+}*/
+
+int main()
+{
+//	ft_printf("%d", printf("aakydktucjfxsnh%{vih"));
+//	printf("%d", printf("kydktucjfxsnh%{kdsvih"));
+	
+/*	ft_printf("%d", printf("qwerty%{kkdkk"));
+	printf("\n\n\n");
+	printf("%d", printf("qwerty%{kkdkk"));*/
+
+
+    ft_printf("%d", printf("aakydktucjfxsnh%{vih"));
+       printf("%d", printf("kydktucjfxsnh%{kdvih"));
+}
